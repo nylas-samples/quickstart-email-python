c08ad3da295309f67ed10426a662d650075ae7e4
Blag <atejada@gmail.com>
2024-02-08 11:36:50 -0500

    Add source code
    Add source code for the Flask project

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# quickstart-email-python
-Quickstart Email Guide for Python
+# How to run
+
+1. Install the dependencies
+
+```bash
+pip3 install Flask, dotenv
+```
+
+2. Run the project
+
+```bash
+python quickstart-email-ruby.py
+```
+
+3. In the Nylas dashboard, create a new application and set the Google connector redirect URL to `http://localhost:5000/oauth/exchange`
+
+4. env variables
+
+```env
+NYLAS_CLIENT_ID=""
+NYLAS_API_KEY=""
+NYLAS_API_URI="https://api.us.nylas.com""
+EMAIL="<RECIPIENT_EMAIL_ADDRESS_HERE>"
+```
+
+5. Open your browser and go to `http://localhost:5000/nylas/auth` and log in and end user account
+
+6. After authenticating an end user account, you can visit the following URLs to get a feel for some of what you can do with the Nylas Email API.
+
+```text
+http://localhost:5000/nylas/recent-emails
+http://localhost:5000/nylas/send-email
+```
---
+++ quickstart-email-python.py
@@ -0,0 +1,74 @@
+# Import your dependencies
+from dotenv import load_dotenv
+import os
+from nylas import Client
+from flask import Flask, request, redirect, url_for, session, jsonify
+from flask_session.__init__ import Session
+from nylas.models.auth import URLForAuthenticationConfig
+from nylas.models.auth import CodeExchangeRequest
+
+# Load your env variables
+load_dotenv()
+
+# Create the app
+app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Initialize Nylas client
+nylas = Client(
+    api_key = os.environ.get("V3_API_KEY")
+)
+
+# Call the authorization page
+@app.route("/oauth/exchange", methods=["GET"])
+def authorized():
+    if session.get("grant_id") is None:
+        code = request.args.get("code")
+        exchangeRequest = CodeExchangeRequest({"redirect_uri": "http://localhost:5000/oauth/exchange",
+                                                                                  "code": code,
+                                                                                  "client_id": os.environ.get("V3_CLIENT")})
+        exchange = nylas.auth.exchange_code_for_token(exchangeRequest)
+        session["grant_id"] = exchange.grant_id
+        return redirect(url_for("login"))
+
+# Main page
+@app.route("/nylas/auth", methods=["GET"])
+def login():
+    if session.get("grant_id") is None:
+        config = URLForAuthenticationConfig({"client_id": os.environ.get("V3_CLIENT"), 
+                                                                      "redirect_uri" : "http://localhost:5000/oauth/exchange",
+                                                                      })
+        url = nylas.auth.url_for_oauth2(config)
+        print(url)
+        return redirect(url)
+    else:
+        return f'{session["grant_id"]}'
+
+@app.route("/nylas/read-emails", methods=["GET"])
+def read_emails():
+    query_params = {"limit": 5} 
+    try:
+        messages, _, _ = nylas.messages.list(session["grant_id"], query_params)
+        return jsonify(messages)
+    except Exception as e:
+        return f'{e}'
+
+@app.route("/nylas/send-email", methods=["GET"])
+def send_email():
+    try:
+        body = {"subject" : "Your Subject Here", 
+                     "body":"Your Email Here",
+                     "reply_to":[{"name": "Name", "email": os.environ.get("EMAIL")}],
+                     "to":[{"name": "Name", "email": os.environ.get("EMAIL")}]}
+
+        message = nylas.messages.send(session["grant_id"], request_body = body).data
+
+        return jsonify(message)
+    except Exception as e:
+        return f'{e}'
+
+# Run our application
+if __name__ == "__main__":
+    app.run()
